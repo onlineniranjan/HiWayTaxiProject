ce6a5b9d7ddb5682609d95c62815ee006bd67a4f
niranjan <online.niranjan@gmail.com>
2014-09-14 23:49:20 -0400

    Committing new project

---
+++ HiWayTaxi/src/main/java/com/hiwaytaxi/controller/TaxiBasic.java
@@ -0,0 +1,20 @@
+package com.hiwaytaxi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+public class TaxiBasic {
+	String message = "Welcome to Spring MVC!";
+	 
+	@RequestMapping("/SendMessage")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in Taxi Basic controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
